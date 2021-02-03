eaa4871f422b84b6cc5dab18179e0d96695d016c
Thiébaud Modoux <thiebaud.modoux@gmail.com>
2021-02-03 09:09:34 +0100

    Commentaire pour question 4.

--- controller.ts
+++ controller.ts
@@ -3,8 +3,10 @@
  * Covid API: https://documenter.getpostman.com/view/10877427/SzYW2f8n?version=latest#287c16a2-d2b3-4de7-a45e-0455642c1a92
  */
 
+import axios from 'axios';
+
 interface Covid {
-    
+
 }
 
 interface Country {
@@ -12,12 +14,27 @@ interface Country {
 }
 
 class Controller {
+    covid: any;
+
     /**
      * Display the country with the most covid-19 cases
      * The display can be a simple object/array
      */
     countryWithMostCases() {
-        console.log("1) Country with most cases: ");
+
+        axios.get("https://covid19-api.org/api/status").then((res: any) => {
+            this.covid = res.data;
+            let max = 0;
+            let country;
+            for (let covid of this.covid) {
+                if (covid.cases > max) {
+                    max = covid.cases;
+                    country = covid.country;
+                }
+            }
+            console.log("1) Country with most cases: " + country);
+        });
+
     }
 
 
@@ -26,7 +43,23 @@ class Controller {
      * Display: country's capital (country's name)
      */
     capitalOfCountryWithLessCases() {
-        console.log("\n2) Capital of country with less cases: ", ` () `);
+        axios.get("https://covid19-api.org/api/status").then((res: any) => {
+            this.covid = res.data;
+            this.covid.sort((a: any, b: any) => {
+                return a.cases - b.cases;
+            });
+
+            axios.get("https://restcountries.eu/rest/v2/alpha/" + this.covid[0].country).then((res) => {
+                console.log("\n2) Capital of country with less cases: ", ` (this.covid[0]) `, res.data.capital);
+            });
+
+        });
+    }
+
+    countryNames(countries: any) {
+        return countries.map((x: any) => {
+            return x.name;
+        });
     }
 
     /**
@@ -34,7 +67,15 @@ class Controller {
      * The display must be nice, not a simple object/array (for example A, B, C ...)
      */
     async findCountriesWithoutCovidData() {
-        console.log("\n3) Countries without covid data: ");
+        const allCountries = (await axios.get("https://restcountries.eu/rest/v2/all")).data;
+        const covidCountries = (await axios.get("https://covid19-api.org/api/countries")).data;
+
+        const allCountriesNames = this.countryNames(allCountries);
+        const covidCountriesNames = this.countryNames(covidCountries);
+
+        const diff = allCountriesNames.filter((x: any) => covidCountriesNames.indexOf(x) === -1);
+
+        console.log("\n3) Countries without covid data: " + diff);
     }
 
     /**
@@ -43,9 +84,15 @@ class Controller {
      * When you do a GET on localhost:5000/api/capitalToCovidCases this method must be called
      * This method will take as query parameter a country capital name and you need to return the number of death for this capital's country
      */
-    async capitalToCovidCases(req: Request, res: Response) {
-        
-    }
+    // async capitalToCovidCases(req: any, res: any) {
+    //     const capital = req.query.capital;
+    //     const country = (await axios.get(`https://restcountries.eu/rest/v2/capital/${capital}`)).data;
+
+    //     const covidData = (await axios.get(`https://covid19-api.org/api/status/${country.alpha2Code}`)).data;
+    //     const deaths = covidData.deaths;
+    //     return deaths;
+
+    // }
 }
 
 export default Controller;
--- server.ts
+++ server.ts
@@ -1,16 +1,40 @@
 import Controller from './controller';
+import express from 'express';
+import bodyParser from 'body-parser';
+import axios from 'axios';
 
 const controller = new Controller();
 controller.countryWithMostCases();
 controller.capitalOfCountryWithLessCases();
 controller.findCountriesWithoutCovidData();
 
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(5000, () => {
+  console.log("Server started");
+});
+
+// J'AI COMMENTE LA QUESTION 4 CAR JE L'AI CONTINUEE APRES L'ENTRETIEN
 // for the question 4
 // 
 // GET: '/api/capitalToCovidCases' => controller.capitalToCovidCases();
-// 
+
+// app.get('/api/capitalToCovidCases', (req, res, next) => {
+//   controller.capitalToCovidCases(req, res).then((deaths) => {
+//     res.json({ deaths: deaths });
+//   }).catch(next);
+// });
+
+// // Error handling middleware
+// app.use((err: any, req: any, res: any, next: any) => {
+//   console.error(err);
+//   return res.status(500).json({ error: err.toString() });
+// });
+
 // axios.get('http://localhost:5000/api/capitalToCovidCases?capital=Bern', {
-//     headers: {
-//         'Content-Type': 'application/json',
-//     }
-// }).then(r => console.log("\n4) Death in Switzerland: ", r.data))
+//   headers: {
+//     'Content-Type': 'application/json',
+//   }
+// }).then((r: any) => console.log("\n4) Death in Switzerland: ", r.data));
